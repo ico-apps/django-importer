76c909e3a8a7ba135d33ecfe792844e54a914006
Santiago Lamora <slamora@users.noreply.github.com>
2020-04-30 08:41:25 +0200

    Move back to ico_monitoring code that is not generic

--- djimporter/models.py
+++ djimporter/models.py
@@ -1,12 +1,7 @@
-import json
-
 from django.db import models
 
-# TODO(@slamora) remove dependency with ico_monitoring
-from ico_monitoring.models import MonitoringScheme
 
-
-class Log(models.Model):
+class AbstractBaseLog(models.Model):
     concept = models.CharField(max_length=50)
     status = models.CharField(max_length=25)
     created_at = models.DateTimeField(auto_now=True)
@@ -14,17 +9,3 @@ class Log(models.Model):
 
     class Meta:
         abstract = True
-
-
-class ImportLog(Log):
-    errors = models.TextField(null=True)
-    num_rows = models.IntegerField(null=True)
-    input_file = models.CharField(max_length=100)
-    monitoringscheme = models.ForeignKey(
-        MonitoringScheme,
-        on_delete=models.SET_NULL,
-        null=True
-    )
-
-    def list_errors(self):
-        return json.loads(self.errors or "[]")
--- djimporter/serializers.py
+++
@@ -1,8 +0,0 @@
-import json
-from django.core import serializers
-
-def serialize(element):
-    return serializers.serialize('json', [element])
-
-def deserialize(element):
-    return [x.object for x in serializers.deserialize("json", element)][0]
