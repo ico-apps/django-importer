10269a5859b60cf3287fc98ddd1f4eb1bc50c4fc
Mònica Ramírez Arceda <monica@probeta.net>
2020-11-13 13:29:01 +0100

    Squash progress migrations

--- djimporter/migrations/0003_enlarge_input_field.py
+++ djimporter/migrations/0003_enlarge_input_field.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('djimporter', '0003_auto_20201113_1148'),
+        ('djimporter', '0002_importlog_progress'),
     ]
 
     operations = [
