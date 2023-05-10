1f6eed464d62bc106ff4ed1786d0ff70ddd188b9
Santiago Lamora <santiago@ribaguifi.com>
2023-05-10 11:58:54 +0200

    Fix flake8 warnings

--- djimporter/views.py
+++ djimporter/views.py
@@ -45,7 +45,7 @@ class ImportLogGetView(View):
 
     def get(self, request, *args, **kwargs):
         import_log = ImportLog.objects.get(pk=self.kwargs['pk'])
-        return JsonResponse({'status':import_log.status, 'id':self.kwargs['pk']}, safe=False)
+        return JsonResponse({'status': import_log.status, 'id': self.kwargs['pk']}, safe=False)
 
 
 class ImportDeleteView(DeleteView):
